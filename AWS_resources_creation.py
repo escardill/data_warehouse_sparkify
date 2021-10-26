1b2faf28c64d6e3142dc632b8f6d30dcd1ccc8c6
escardill <nil.celisfont@actuatech.ad>
2021-10-26 14:33:28 +0200

    Some changes

---
+++ AWS_resources_creation.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import boto3
+import json
+
+# Creating a new IAM user
--- create_tables.py
+++ create_tables.py
@@ -1,6 +1,7 @@
 import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
+import boto3
 
 
 def drop_tables(cur, conn):
